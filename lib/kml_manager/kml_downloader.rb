cce21887aea7308eeedbebad071a10c7fadb5eed
Franco Reyes <freyes1988@gmail.com>
2015-02-07 16:09:17 -0600

    Add KML Downloader.
    KML Downloader is used to download KML files. Actually it's kinda
    generic-y so could probably be used for other downloaded files. It'll
    also make a backup of a file if it exists with the same name. It also
    makes you breakfast.... not really.

---
+++ lib/kml_manager/kml_downloader.rb
@@ -0,0 +1,48 @@
+require 'pathname'
+require 'open-uri'
+require 'diffy'
+
+module KMLManager
+  class KMLDownloader
+    def self.download_KML(url, file_name)
+      file_path = base_directory.join(file_name)
+      download_into_temp_file(url)
+
+      if file_path.exist?
+        diff = Diffy::Diff.new(file_path.to_s, temp_file.to_s, source: 'files')
+        if diff.count > 0
+          backup_file(file_path)
+        end
+      end
+
+      move_temp_to_filename(file_path)
+      Pathname.new(file_path)
+    end
+
+    private
+
+    def self.download_into_temp_file(url)
+      open(temp_file.to_s, 'w') do |io|
+        open(url) do |response|
+          io.write(response.read)
+        end
+      end
+    end
+
+    def self.temp_file
+      base_directory.join('downloaded_kml.tmp')
+    end
+
+    def self.move_temp_to_filename(target)
+      File.rename temp_file, target
+    end
+
+    def self.base_directory
+      Rails.root.join('lib', 'data')
+    end
+
+    def self.backup_file(original_file)
+      File.rename original_file, "#{original_file}.bak"
+    end
+  end
+end
