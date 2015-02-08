2f2158e3305eb28a95dc73916e49a07094984d79
Franco Reyes <freyes1988@gmail.com>
2015-02-08 00:05:55 -0600

    Create Test for KML Downloader.
    Created RSpec tests for KML Downloader portion of KML Manager. Also
    added VCR configuration file.

---
+++ spec/kml_manager/kml_downloader_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+describe KMLManager::KMLDownloader do
+  let (:cta_rail_kml_url)    { "https://data.cityofchicago.org/download/m3d6-pubu/application/xml" }
+  let (:cta_station_kml_url) { "https://data.cityofchicago.org/download/bs96-uama/application/xml" }
+
+  let (:cta_rail_file_name) { "cta_rail_lines" }
+  let (:cta_station_file_name) { "cta_train_stations" }
+
+  it "Downloads a file for cta rail KML data", :vcr do
+    downloaded_file_path = KMLManager::KMLDownloader.
+      download_KML(cta_rail_kml_url, cta_rail_file_name)
+
+    expect(downloaded_file_path.exist?).to be_truthy
+    expect(downloaded_file_path.file?).to be_truthy
+  end
+
+  it "Downloads a file for cta station KML data", :vcr do
+    downloaded_file_path = KMLManager::KMLDownloader.
+      download_KML(cta_station_kml_url, cta_station_file_name)
+
+    expect(downloaded_file_path.exist?).to be_truthy
+    expect(downloaded_file_path.file?).to be_truthy
+  end
+
+  it "Creates a backup of older file if different", :vcr do
+    # To simulate a different file we're going to download the rail KML file
+    # under the station KML file name
+
+    backup_file_name= cta_station_file_name + ".bak"
+
+    [cta_station_kml_url, cta_rail_kml_url].each do |url|
+      KMLManager::KMLDownloader.download_KML(url, cta_station_file_name)
+    end
+
+    expect(backup_file_name.exist?).to be_truthy
+    expect(backup_file_name.file?).to be_truthy
+  end
+
+  it "Keeps an older version if file changed", :vcr do
+    backup_file_name= cta_station_file_name + ".bak"
+
+    [cta_station_kml_url, cta_station_kml_url].each do |url|
+      KMLManager::KMLDownloader.download_KML(url, cta_station_file_name)
+    end
+
+    expect(backup_file_name.exist?).to be_falsey
+    expect(backup_file_name.file?).to be_falsey
+  end
+end
