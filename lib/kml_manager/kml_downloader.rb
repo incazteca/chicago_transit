46ebb73bd33c81a06c955a65200a0db9b5381e85
Franco Reyes <freyes1988@gmail.com>
2015-03-03 01:05:01 -0600

    Have KML Downloader download KML files.

--- lib/kml_manager/kml_downloader.rb
+++ lib/kml_manager/kml_downloader.rb
@@ -1,22 +1,20 @@
+require 'pathname'
 require 'open-uri'
 
 module KMLManager
   class KMLDownloader
     def self.download_KML(url, file_name)
       base_directory = Rails.root.join('lib','data')
-      #uri = URI(url)
 
       file_path = base_directory.join(file_name)
 
-      URI.parse(url).read do |response|
-        open file_path.to_s, 'w' do |io|
-          response.read_body do |chunk|
-            io.write chunk
-          end
+      open(file_path.to_s, 'w') do |io|
+        open(url) do |response|
+          io.write(response.read)
         end
       end
 
-      file_path
+      Pathname.new(file_path)
     end
   end
 end
